ec7fffcacd384d688afe9d21d78b49db86605e9f
lucasdsks <lucasdrifitim@hotmail.com>
2021-08-31 18:23:54 -0300

    Solução do Uri 1003 em Java

---
+++ src/ExerciciosUri/Ex1003.java
@@ -0,0 +1,17 @@
+package ExerciciosUri;
+
+import java.util.Scanner;
+
+public class Ex1003 {
+	
+	public static void main(String[] args) {
+		 Scanner sc = new Scanner(System.in);
+		 
+		 int a = sc.nextInt();
+		 int b = sc.nextInt();
+		 
+		 System.out.println("SOMA = " + (a+b));		 
+		 sc.close();
+	}
+
+}
--- src/ExerciciosUri/Ex1006.java
+++ src/ExerciciosUri/Ex1006.java
@@ -14,7 +14,7 @@ public class Ex1006 {
 		double a = sc.nextDouble();
 		double b = sc.nextDouble();
 		double c = sc.nextDouble();
-		double media = ((a*2)+(b*3)+(c*5))/10;
+		double media = (a*2+b*3+c*5)/10;
 		System.out.printf("MEDIA = %.1f\n", media);
 		sc.close();
 	}
